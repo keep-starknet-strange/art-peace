5c5a31fde70cecf2435982acbe3edb7454b2cdf5
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2024-04-16 23:31:48 +0100

    feat: fixed PR comments

--- frontend/src/tabs/Account.js
+++ frontend/src/tabs/Account.js
@@ -1,17 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Account.css";
 import BasicTab from "./BasicTab.js";
 
 const Account = (props) => {
   // TODO: Create the account tab w/ wallet address, username, pixel info, top X % users ( pixels placed? ), ...
   const [username, setUsername] = useState("");
+  const [pixelCount, setPixelCount] = useState(2572);
+  const [accountRank, setAccountRank] = useState("");
   const [isUsernameSaved, saveUsername] = useState(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(event);
     setUsername(username);
     saveUsername(true);
+  };
+
+  const editUsername = (e) => {
+    saveUsername(false);
   }
+
+  useEffect(() => {
+    if (pixelCount >= 5000) {
+      setAccountRank("Champion");
+    } else if (pixelCount >= 3000) {
+      setAccountRank("Platinum");
+    } else if (pixelCount >= 2000) {
+      setAccountRank("Gold");
+    } else if (pixelCount >= 1000) {
+      setAccountRank("Silver");
+    } else {
+      setAccountRank("Bronze");
+    }
+  });
   return (
     <BasicTab title="Account">
       <div className="Account__flex">
@@ -23,9 +43,14 @@ const Account = (props) => {
       <div className="Account__flex Account__flex--center">
         <p>Username:</p>
         {isUsernameSaved ? (
-          <p>{username}</p>
+          <div className="Account__user">
+            <p>{username}</p>
+            <button className="Account__button Account__button--edit" type="button" onClick={editUsername}>
+              edit
+            </button>
+          </div>
         ) : (
-          <form onSubmit={handleSubmit}>
+          <form className="Account__user" onSubmit={handleSubmit}>
             <label>
               <input
                 className="Account__input"
@@ -35,23 +60,19 @@ const Account = (props) => {
                 onChange={(e) => setUsername(e.target.value)}
               />
             </label>
-            <button className="Account__button" type="submit">update</button>
+            <button className="Account__button Account__button--submit" type="submit">
+              update
+            </button>
           </form>
         )}
       </div>
       <div className="Account__flex">
         <p>Pixel count:</p>
-        <p>572</p>
+        <p>{pixelCount}</p>
       </div>
       <div className="Account__flex">
-        <p>Top 5 🔥</p>
-        <ul className="Account__list">
-          <li>0x20579</li>
-          <li>0x00918</li>
-          <li>0x55603</li>
-          <li>0x07802</li>
-          <li>0x09166</li>
-        </ul>
+        <p>Current Rank:</p>
+        <p>{accountRank}</p>
       </div>
     </BasicTab>
   );
