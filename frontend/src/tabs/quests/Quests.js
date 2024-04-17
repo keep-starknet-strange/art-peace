846b85c0a3f5f633d0a4a06de98f8c0fe40a6e36
Otaiki1 <otaikisadiq2804@gmail.com>
2024-04-17 01:40:02 +0100

    add sorting for completed

--- frontend/src/tabs/quests/Quests.js
+++ frontend/src/tabs/quests/Quests.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import './Quests.css';
-import BasicTab from '../BasicTab.js';
-import QuestItem from './QuestItem.js';
+import React from "react";
+import "./Quests.css";
+import BasicTab from "../BasicTab.js";
+import QuestItem from "./QuestItem.js";
 
-const Quests = props => {
+const Quests = (props) => {
   // TODO: Main quests should be scrollable
   // TODO: Main quests should be moved to the bottom on complete
   // TODO: Pull quests from backend
@@ -13,62 +13,86 @@ const Quests = props => {
       title: "Place 10 pixels",
       description: "Add 10 pixels on the canvas",
       reward: "3",
-      status: "completed"
+      status: "completed",
     },
     {
       title: "Build a template",
       description: "Create a template for the community to use",
       reward: "3",
-      status: "claim"
+      status: "claim",
     },
     {
       title: "Deploy a Memecoin",
       description: "Create an Unruggable memecoin",
       reward: "10",
-      status: "completed"
-    }
-  ]
+      status: "completed",
+    },
+  ];
 
   const mainQuests = [
     {
       title: "Tweet #art/peace",
       description: "Tweet about art/peace using the hashtag & addr",
       reward: "10",
-      status: "incomplete"
+      status: "incomplete",
     },
     {
       title: "Place 100 pixels",
       description: "Add 100 pixels on the canvas",
       reward: "10",
-      status: "completed"
+      status: "completed",
     },
     {
       title: "Mint an art/peace NFT",
       description: "Mint an NFT using the art/peace theme",
       reward: "5",
-      status: "incomplete"
+      status: "incomplete",
+    },
+  ];
+
+  const sortByCompleted = (arr) => {
+    const newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].status == "completed") {
+        newArray.push(arr[i]);
+      } else {
+        newArray.unshift(arr[i]);
+      }
     }
-  ]
+    return newArray;
+  };
 
   // TODO: Icons for each tab?
   return (
     <BasicTab title="Quests">
-      <div style={{height: '70vh', overflowY: 'scroll'}}>
-        <div style={{display: 'flex', alignItems: 'center'}}>
+      <div style={{ height: "70vh", overflowY: "scroll" }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
           <h2 className="Quests__item__header">Dailys</h2>
-          <p style={{fontSize: "1rem", marginLeft: "1rem"}}>XX:XX:XX</p>
+          <p style={{ fontSize: "1rem", marginLeft: "1rem" }}>XX:XX:XX</p>
         </div>
-        {dailyQuests.map((quest, index) => (
-          <QuestItem key={index} title={quest.title} description={quest.description} reward={quest.reward} status={quest.status} />
+        {sortByCompleted(dailyQuests).map((quest, index) => (
+          <QuestItem
+            key={index}
+            title={quest.title}
+            description={quest.description}
+            reward={quest.reward}
+            status={quest.status}
+          />
         ))}
-          
+
         <h2 className="Quests__item__header">Main</h2>
-        {mainQuests.map((quest, index) => (
-          <QuestItem key={index} title={quest.title} description={quest.description} reward={quest.reward} status={quest.status} />
+        {sortByCompleted(mainQuests).map((quest, index) => (
+          <QuestItem
+            key={index}
+            title={quest.title}
+            description={quest.description}
+            reward={quest.reward}
+            status={quest.status}
+          />
         ))}
       </div>
     </BasicTab>
   );
-}
+};
 
 export default Quests;
