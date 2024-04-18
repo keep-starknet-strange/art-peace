2491ff7f4176e1e611c193394840c7606d9bced8
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2024-04-18 16:41:12 +0100

    feat: formatted file

--- backend/routes/canvas.go
+++ backend/routes/canvas.go
@@ -14,29 +14,29 @@ func InitCanvasRoutes() {
 }
 
 func initCanvas(w http.ResponseWriter, r *http.Request) {
-    // Check if the 'canvas' property exists in core.ArtPeaceBackend.CanvasConfig
-    if core.ArtPeaceBackend.CanvasConfig != nil && core.ArtPeaceBackend.CanvasConfig.Canvas != nil {
-        // Calculate totalByteSize
-        totalBitSize := core.ArtPeaceBackend.CanvasConfig.Canvas.Width * core.ArtPeaceBackend.CanvasConfig.Canvas.Height * core.ArtPeaceBackend.CanvasConfig.ColorsBitWidth
-        totalByteSize := (totalBitSize / 8)
-        if totalBitSize%8 != 0 {
-            // Round up to nearest byte
-            totalByteSize += 1
-        }
+	// Check if the 'canvas' property exists in core.ArtPeaceBackend.CanvasConfig
+	if core.ArtPeaceBackend.CanvasConfig != nil && core.ArtPeaceBackend.CanvasConfig.Canvas != nil {
+		// Calculate totalByteSize
+		totalBitSize := core.ArtPeaceBackend.CanvasConfig.Canvas.Width * core.ArtPeaceBackend.CanvasConfig.Canvas.Height * core.ArtPeaceBackend.CanvasConfig.ColorsBitWidth
+		totalByteSize := (totalBitSize / 8)
+		if totalBitSize%8 != 0 {
+			// Round up to nearest byte
+			totalByteSize += 1
+		}
 
-        // Create canvas
-        canvas := make([]byte, totalByteSize)
-        ctx := context.Background()
-        err := core.ArtPeaceBackend.Databases.Redis.Set(ctx, "canvas", canvas, 0).Err()
-        if err != nil {
-            panic(err)
-        }
+		// Create canvas
+		canvas := make([]byte, totalByteSize)
+		ctx := context.Background()
+		err := core.ArtPeaceBackend.Databases.Redis.Set(ctx, "canvas", canvas, 0).Err()
+		if err != nil {
+			panic(err)
+		}
 
-        fmt.Println("Canvas initialized")
-    } else {
-        fmt.Println("Canvas config not found")
-        // Handle the case where 'canvas' property does not exist
-    }
+		fmt.Println("Canvas initialized")
+	} else {
+		fmt.Println("Canvas config not found")
+		// Handle the case where 'canvas' property does not exist
+	}
 }
 
 func getCanvas(w http.ResponseWriter, r *http.Request) {
