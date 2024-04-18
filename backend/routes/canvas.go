e9cbf38172a9da8e336ea0aa9ef8963d5ff48d77
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2024-04-18 15:45:29 +0100

    feat: added check to prevent canvas re-init

--- backend/routes/canvas.go
+++ backend/routes/canvas.go
@@ -14,23 +14,31 @@ func InitCanvasRoutes() {
 }
 
 func initCanvas(w http.ResponseWriter, r *http.Request) {
-	// TODO: Check if canvas already exists
-	totalBitSize := core.ArtPeaceBackend.CanvasConfig.Canvas.Width * core.ArtPeaceBackend.CanvasConfig.Canvas.Height * core.ArtPeaceBackend.CanvasConfig.ColorsBitWidth
-	totalByteSize := (totalBitSize / 8)
-	if totalBitSize%8 != 0 {
-		// Round up to nearest byte
-		totalByteSize += 1
-	}
+    // Check if the 'canvas' property exists in core.ArtPeaceBackend.CanvasConfig
+    if core.ArtPeaceBackend.CanvasConfig != nil && core.ArtPeaceBackend.CanvasConfig.Canvas != nil {
+        // Calculate totalByteSize
+        totalBitSize := core.ArtPeaceBackend.CanvasConfig.Canvas.Width * core.ArtPeaceBackend.CanvasConfig.Canvas.Height * core.ArtPeaceBackend.CanvasConfig.ColorsBitWidth
+        totalByteSize := (totalBitSize / 8)
+        if totalBitSize%8 != 0 {
+            // Round up to nearest byte
+            totalByteSize += 1
+        }
 
-	canvas := make([]byte, totalByteSize)
-	ctx := context.Background()
-	err := core.ArtPeaceBackend.Databases.Redis.Set(ctx, "canvas", canvas, 0).Err()
-	if err != nil {
-		panic(err)
-	}
+        // Create canvas
+        canvas := make([]byte, totalByteSize)
+        ctx := context.Background()
+        err := core.ArtPeaceBackend.Databases.Redis.Set(ctx, "canvas", canvas, 0).Err()
+        if err != nil {
+            panic(err)
+        }
 
-	fmt.Println("Canvas initialized")
+        fmt.Println("Canvas initialized")
+    } else {
+        fmt.Println("Canvas config not found")
+        // Handle the case where 'canvas' property does not exist
+    }
 }
+
 
 func getCanvas(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
