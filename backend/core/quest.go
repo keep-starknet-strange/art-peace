9117b0520205176e390616874543cd13237f5ec5
Fishon Amos <43862685+fishonamos@users.noreply.github.com>
2024-04-18 22:26:38 -0700

    Update quest.go

--- backend/core/quest.go
+++ backend/core/quest.go
@@ -1,11 +1,1 @@
-package core
 
-// Quest represents a quest from the database
-type Quest struct {
-  ID           int    `json:"id"`
-  Name         string `json:"name"`
-  Description  string `json:"description"`
-  Reward       int    `json:"reward"`
-  DayIndex     int    `json:"dayIndex"`
-  Completed    int    `json:"completed"`
-}
