a5cb11a499e8e205c5816d4ddd06671ef8924cd4
Fishon Amos <43862685+fishonamos@users.noreply.github.com>
2024-04-18 22:04:31 -0700

    Update routes.go

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,17 +1,12 @@
 package routes
 
-import (
-  "github.com/gorilla/mux"
-)
-
-func InitRoutes(router *mux.Router) { // THis will accepts the router instance as an argument
-  InitIndexerRoutes(router)
-  InitCanvasRoutes(router)
-  InitQuestsRoutes(router)
-  InitPixelRoutes(router)
-  InitWebsocketRoutes(router)
-  InitTemplateRoutes(router)
-  InitUserRoutes(router)
-  InitContractRoutes(router)
+func InitRoutes() {
+	InitIndexerRoutes()
+	InitCanvasRoutes()
+	InitPixelRoutes()
+	InitWebsocketRoutes()
+	InitTemplateRoutes()
+	InitUserRoutes()
+	InitContractRoutes()
+	InitQuestsRoutes()
 }
-
