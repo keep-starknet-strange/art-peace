fd951ce67f1f5e85e039608f310104db3165d323
raizo07 <agadaraizo@gmail.com>
2024-04-23 18:54:37 +0100

    feat: import websocket from the backend.config.json file

--- backend/routes/websocket.go
+++ backend/routes/websocket.go
@@ -2,20 +2,46 @@ package routes
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"encoding/json"
 
 	"github.com/gorilla/websocket"
 
 	"github.com/keep-starknet-strange/art-peace/backend/core"
 )
 
-func InitWebsocketRoutes() {
-	http.HandleFunc("/ws", wsEndpoint)
+type Config struct {
+	Host      string `json:"host"`
+	Port      int    `json:"port"`
+	WebSocket struct {
+		ReadBufferSize  int `json:"ReadBufferSize"`
+		WriteBufferSize int `json:"WriteBufferSize"`
+	} `json:"WebSocket"`
 }
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+func LoadConfig() (Config, error) {
+	var config Config
+
+	configFile, err := os.Open("../config/backend.config.json")
+
+	if err != nil {
+		return config, err
+	}
+	defer configFile.Close()
+
+	jsonParser := json.NewDecoder(configFile)
+	if err = jsonParser.Decode(&config); err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
+
+func InitWebsocketRoutes() {
+	http.HandleFunc("/ws", wsEndpoint)
 }
 
 func wsReader(conn *websocket.Conn) {
@@ -31,6 +57,16 @@ func wsReader(conn *websocket.Conn) {
 }
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	config, err := LoadConfig()
+
+	if err != nil {
+		log.Fatal("Error loading config file: ", err)
+	}
+
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  config.WebSocket.ReadBufferSize,
+		WriteBufferSize: config.WebSocket.WriteBufferSize,
+	}
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
 	ws, err := upgrader.Upgrade(w, r, nil)
