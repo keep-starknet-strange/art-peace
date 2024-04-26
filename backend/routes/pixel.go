8d44c71a89e8321e31593c87a82a7759fcd9a023
Xaxxoo <51526246+Xaxxoo@users.noreply.github.com>
2024-04-26 16:25:00 +0100

    Chore route cleanup (#63)
    * route cleanup
    
    * Few missed routes
    
    ---------
    
    Co-authored-by: Brandon R <54774639+b-j-roberts@users.noreply.github.com>
    Co-authored-by: Brandon Roberts <brandonjroberts22@gmail.com>

--- backend/routes/pixel.go
+++ backend/routes/pixel.go
@@ -14,13 +14,13 @@ import (
 )
 
 func InitPixelRoutes() {
-	http.HandleFunc("/getPixel", getPixel)
-	http.HandleFunc("/getPixelInfo", getPixelInfo)
+	http.HandleFunc("/get-pixel", getPixel)
+	http.HandleFunc("/get-pixel-info", getPixelInfo)
 	if !core.ArtPeaceBackend.BackendConfig.Production {
-		http.HandleFunc("/placePixelDevnet", placePixelDevnet)
-		http.HandleFunc("/placeExtraPixelsDevnet", placeExtraPixelsDevnet)
+		http.HandleFunc("/place-pixel-devnet", placePixelDevnet)
+		http.HandleFunc("/place-extra-pixels-devnet", placeExtraPixelsDevnet)
 	}
-	http.HandleFunc("/placePixelRedis", placePixelRedis)
+	http.HandleFunc("/place-pixel-redis", placePixelRedis)
 }
 
 func getPixel(w http.ResponseWriter, r *http.Request) {
