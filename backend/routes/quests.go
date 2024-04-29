8d3551a3fd669fd322cc36cdaf1eb7ecd40d872c
Brandon Roberts <brandonjroberts22@gmail.com>
2024-04-29 18:12:28 -0500

    Full integration of new days from onchain interaction to app

--- backend/routes/quests.go
+++ backend/routes/quests.go
@@ -53,7 +53,7 @@ func GetMainQuests(w http.ResponseWriter, r *http.Request) {
 
 // Get today's quests based on the current day index.
 func getTodaysQuests(w http.ResponseWriter, r *http.Request) {
-	quests, err := core.PostgresQueryJson[DailyQuest]("SELECT key, name, description, reward, dayIndex FROM DailyQuests WHERE dayIndex = (SELECT MAX(dayIndex) FROM Days)")
+	quests, err := core.PostgresQueryJson[DailyQuest]("SELECT key, name, description, reward, day_index FROM DailyQuests WHERE day_index = (SELECT MAX(day_index) FROM Days)")
 	if err != nil {
 		WriteErrorJson(w, http.StatusInternalServerError, "Failed to get today's quests")
 		return
@@ -86,7 +86,7 @@ func GetCompletedDailyQuests(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	quests, err := core.PostgresQueryJson[DailyQuest]("SELECT key, name, description, reward, dayIndex FROM DailyQuests WHERE key = (SELECT questKey FROM UserDailyQuests WHERE userAddress = $1 AND completed = TRUE)", userAddress)
+	quests, err := core.PostgresQueryJson[DailyQuest]("SELECT key, name, description, reward, day_index FROM DailyQuests WHERE key = (SELECT questKey FROM UserDailyQuests WHERE userAddress = $1 AND completed = TRUE)", userAddress)
 	if err != nil {
 		WriteErrorJson(w, http.StatusInternalServerError, "Failed to get completed daily quests")
 		return
@@ -96,12 +96,12 @@ func GetCompletedDailyQuests(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetTodayStartTime(w http.ResponseWriter, r *http.Request) {
-  todayStartTime, err := core.PostgresQueryOne[time.Time]("SELECT dayStart FROM days WHERE dayIndex = (SELECT MAX(dayIndex) FROM days)")
+  todayStartTime, err := core.PostgresQueryOne[time.Time]("SELECT day_start FROM days WHERE day_index = (SELECT MAX(day_index) FROM days)")
   if err != nil {
     WriteErrorJson(w, http.StatusInternalServerError, "Failed to get today's start time")
     return
   }
 
-  WriteDataJson(w, string(todayStartTime.UTC().Format(time.RFC3339)))
+  WriteDataJson(w, "\"" + string(todayStartTime.UTC().Format(time.RFC3339)) + "\"")
 }
 
