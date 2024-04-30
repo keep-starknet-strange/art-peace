820c17794225a1caf1aa93a2effbb83f39cd5d5e
Adeyemi <addegbenga@gmail.com>
2024-04-30 16:01:43 +0100

    feat: added style to link

--- frontend/src/utils/encodeToLink.js
+++ frontend/src/utils/encodeToLink.js
@@ -1,8 +1,7 @@
 export const encodeToLink = (description) => {
     // Replace Markdown-style links with actual HTML anchor tags
     const processedDescription = description.replace(/\[(.*?)\]\((.*?)\)/g, (match, text, link) => {
-      return `<a href=${link} target="_blank" rel="noopener noreferrer">${text}</a>`;
+      return `<a class="link_style" href=${link} target="_blank" rel="noopener noreferrer">${text}</a>`;
     });
-  
     return { __html: processedDescription };
 };
