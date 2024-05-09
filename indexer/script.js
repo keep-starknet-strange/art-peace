71c027ddee2b86c6a0bc56489ee5f85c1d74e50e
tekkac <tarik@carbonable.io>
2024-05-09 11:06:25 +0200

    🔨 add ExtraPlaced Event listener in js script

--- indexer/script.js
+++ indexer/script.js
@@ -8,7 +8,19 @@ export const config = {
       {
         // Pixel Placed Event
         fromAddress: Deno.env.get("ART_PEACE_CONTRACT_ADDRESS"),
-        keys: ["0x2D7B50EBF415606D77C7E7842546FC13F8ACFBFD16F7BCF2BC2D08F54114C23"],
+        keys: [
+          "0x2D7B50EBF415606D77C7E7842546FC13F8ACFBFD16F7BCF2BC2D08F54114C23",
+        ],
+        includeReverted: false,
+        includeTransaction: false,
+        includeReceipt: false,
+      },
+      {
+        // Extra Pixels Placed Event
+        fromAddress: Deno.env.get("ART_PEACE_CONTRACT_ADDRESS"),
+        keys: [
+          "0xE8F5C4E6F651BF4C7B093805F85C9B8EC2EC428210F90A4C9C135C347F48C",
+        ],
         includeReverted: false,
         includeTransaction: false,
         includeReceipt: false,
@@ -16,7 +28,9 @@ export const config = {
       {
         // NFT Minted Event
         fromAddress: Deno.env.get("NFT_CONTRACT_ADDRESS"),
-        keys: ["0x30826E0CD9A517F76E857E3F3100FE5B9098E9F8216D3DB283FB4C9A641232F"],
+        keys: [
+          "0x30826E0CD9A517F76E857E3F3100FE5B9098E9F8216D3DB283FB4C9A641232F",
+        ],
         includeReverted: false,
         includeTransaction: false,
         includeReceipt: false,
@@ -24,7 +38,9 @@ export const config = {
       {
         // Template Added Event
         fromAddress: Deno.env.get("ART_PEACE_CONTRACT_ADDRESS"),
-        keys: ["0x3E18EC266FE76A2EFCE73F91228E6E04456B744FC6984C7A6374E417FB4BF59"],
+        keys: [
+          "0x3E18EC266FE76A2EFCE73F91228E6E04456B744FC6984C7A6374E417FB4BF59",
+        ],
         includeReverted: false,
         includeTransaction: false,
         includeReceipt: false,
@@ -32,7 +48,9 @@ export const config = {
       {
         // Vote Color Event
         fromAddress: Deno.env.get("ART_PEACE_CONTRACT_ADDRESS"),
-        keys: ["0x2407C82B0EFA2F6176A075BA5A939D33EEFAB39895FABCF3AC1C5E897974A40"],
+        keys: [
+          "0x2407C82B0EFA2F6176A075BA5A939D33EEFAB39895FABCF3AC1C5E897974A40",
+        ],
         includeReverted: false,
         includeTransaction: false,
         includeReceipt: false,
@@ -40,7 +58,9 @@ export const config = {
       {
         // New Day Event
         fromAddress: Deno.env.get("ART_PEACE_CONTRACT_ADDRESS"),
-        keys: ["0x19CDBD24E137C00D1FEB99CC0B48B86B676F6B69C788C7F112AFEB8CD614C16"],
+        keys: [
+          "0x19CDBD24E137C00D1FEB99CC0B48B86B676F6B69C788C7F112AFEB8CD614C16",
+        ],
         includeReverted: false,
         includeTransaction: false,
         includeReceipt: false,
