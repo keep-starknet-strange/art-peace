46185e907f32048a6017374c1dca2805d16445bd
Fishon Amos <amosfishon@gmail.com>
2024-05-13 22:54:57 +0100

    update cors

--- backend/routes/utils/responses.go
+++ backend/routes/utils/responses.go
@@ -4,20 +4,36 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/websocket"
+	"github.com/keep-starknet-strange/art-peace/backend/config"
 	"github.com/keep-starknet-strange/art-peace/backend/core"
 )
 
+func getHttpConfig() config.HttpConfig {
+	return core.ArtPeaceBackend.BackendConfig.HttpConfigValues
+}
+
 func SetupAccessHeaders(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	config := getHttpConfig()
+
+	// Set Access-Control-Allow-Origin dynamically based on config
+	for _, origin := range config.AllowOrigin {
+		w.Header().Add("Access-Control-Allow-Origin", origin)
+	}
+
+	// Join methods into a single string for the header
+	methods := strings.Join(config.AllowMethods, ", ")
+	w.Header().Set("Access-Control-Allow-Methods", methods)
+
+	// Join headers into a single string for the header
+	headers := strings.Join(config.AllowHeaders, ", ")
+	w.Header().Set("Access-Control-Allow-Headers", headers)
 }
 
 func SetupHeaders(w http.ResponseWriter) {
 	SetupAccessHeaders(w)
-
 	w.Header().Set("Content-Type", "application/json")
 }
 
@@ -41,7 +57,6 @@ func WriteResultJson(w http.ResponseWriter, result string) {
 	w.Write(BasicResultJson(result))
 }
 
-// TODO: To string
 func BasicDataJson(data string) []byte {
 	return []byte(`{"data": ` + data + `}`)
 }
@@ -61,8 +76,7 @@ func SendWebSocketMessage(w http.ResponseWriter, message map[string]interface{})
 	for idx, conn := range core.ArtPeaceBackend.WSConnections {
 		if err := conn.WriteMessage(websocket.TextMessage, messageBytes); err != nil {
 			fmt.Println(err)
-			// TODO: Should we always remove connection?
-			// Remove connection
+			// Remove problematic connection
 			conn.Close()
 			core.ArtPeaceBackend.WSConnections = append(core.ArtPeaceBackend.WSConnections[:idx], core.ArtPeaceBackend.WSConnections[idx+1:]...)
 		}
--- backend/routes/websocket.go
+++ backend/routes/websocket.go
@@ -25,37 +25,20 @@ func wsReader(conn *websocket.Conn) {
 		fmt.Println("WS message received: ", messageType, string(p))
 	}
 }
+
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	upgrader := websocket.Upgrader{
 		ReadBufferSize:  core.ArtPeaceBackend.BackendConfig.WebSocket.ReadBufferSize,
 		WriteBufferSize: core.ArtPeaceBackend.BackendConfig.WebSocket.WriteBufferSize,
-
-		CheckOrigin: func(r *http.Request) bool {
-			origin := r.Header.Get("Origin")
-			if origin == "" {
-				return true
-			}
-
-			for _, allowedOrigin := range core.ArtPeaceBackend.BackendConfig.WebSocket.AllowOrigin {
-				if allowedOrigin == "*" {
-					return true
-				}
-				if origin == allowedOrigin {
-					return true
-				}
-			}
-
-			return false
-		},
-
-		//To do: Logic for Allowed Methods and Allowed headers
-
 	}
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
 	core.ArtPeaceBackend.WSConnections = append(core.ArtPeaceBackend.WSConnections, ws)
 	wsReader(ws)
 }
