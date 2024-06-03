6d5fb74f44c6691b30f32d64eabba6ae264303e6
Joy Adah <99573957+Joy-Adah@users.noreply.github.com>
2024-06-03 23:45:13 +0100

    NFT share button PR (#204)
    * feat: nft share button
    
    * Formatting and slight text change
    
    ---------
    
    Co-authored-by: Brandon Roberts <brandonjroberts22@gmail.com>

--- frontend/src/tabs/nfts/NFTItem.js
+++ frontend/src/tabs/nfts/NFTItem.js
@@ -54,6 +54,12 @@ const NFTItem = (props) => {
   const posy = Math.floor(props.position / canvasConfig.canvas.width);
 
   const [minterText, setMinterText] = React.useState('');
+
+  function handleShare() {
+    const twitterShareUrl = `https://x.com/intent/post?text=${encodeURIComponent('Just minted my #ArtPeace')}&url=${encodeURIComponent(props.image)}`;
+    window.open(twitterShareUrl, '_blank');
+  }
+
   // TODO: Load name from initial query instead of fetching it again
   useEffect(() => {
     async function fetchUsernameUrl() {
@@ -94,7 +100,7 @@ const NFTItem = (props) => {
           />
           <div className='NFTItem__overlay'>
             <div className='NFTItem__buttons'>
-              <div className='NFTItem__button'>
+              <div onClick={handleShare} className='NFTItem__button'>
                 <img className='Share__icon' src={ShareIcon} alt='Share' />
               </div>
               <div
