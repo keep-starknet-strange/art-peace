21b613788079ab47d91c67c99e1a4a5532f76f27
raizo07 <agadaraizo@gmail.com>
2024-06-10 19:24:26 +0100

    fix background color for Notif panel

--- frontend/src/tabs/NotificationPanel.js
+++ frontend/src/tabs/NotificationPanel.js
@@ -1,6 +1,43 @@
+// import React, { useState, useEffect } from 'react';
+// import './NotificationPanel.css';
+
+// const NotificationPanel = ({ message, onClose, animationDuration = 3000 }) => {
+//   const [isVisible, setIsVisible] = useState(false);
+
+//   useEffect(() => {
+//     if (message) {
+//       setIsVisible(true);
+//       const timer = setTimeout(() => {
+//         setIsVisible(false);
+//         if (onClose) onClose();
+//       }, animationDuration);
+//       return () => clearTimeout(timer);
+//     }
+//   }, [message, onClose, animationDuration]);
+
+//   return (
+//     <div
+//       className={`notification-panel ${isVisible ? 'slide-in' : 'slide-out'}`}
+//     >
+//       <div className='notification-content'>
+//         <p>{message}</p>
+//         <p
+//           className='NotificationPanel__close'
+//           onClick={() => {
+//             setIsVisible(false);
+//             if (onClose) onClose();
+//           }}
+//         >
+//           X
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default NotificationPanel;
 import React, { useState, useEffect } from 'react';
 import './NotificationPanel.css';
-// import '../utils/Styles.css'; // Ensure this import is correct
 
 const NotificationPanel = ({ message, onClose, animationDuration = 3000 }) => {
   const [isVisible, setIsVisible] = useState(false);
@@ -16,21 +53,23 @@ const NotificationPanel = ({ message, onClose, animationDuration = 3000 }) => {
     }
   }, [message, onClose, animationDuration]);
 
+  const closeOverlay = () => {
+    setIsVisible(false);
+    if (onClose) onClose();
+  };
+
   return (
     <div
       className={`notification-panel ${isVisible ? 'slide-in' : 'slide-out'}`}
     >
       <div className='notification-content'>
         <p>{message}</p>
-        <p
-          className='NotificationPanel__close'
-          onClick={() => {
-            setIsVisible(false);
-            if (onClose) onClose();
-          }}
+        <div
+          className='TemplateOverlay__close Text__medium'
+          onClick={closeOverlay}
         >
           X
-        </p>
+        </div>
       </div>
     </div>
   );
