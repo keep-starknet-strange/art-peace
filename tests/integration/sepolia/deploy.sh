abc1c29cfc00defb3926740d58c127bd070ff601
Brandon Roberts <brandonjroberts22@gmail.com>
2024-07-08 04:47:22 +0200

    New contract deployments and abis

--- tests/integration/sepolia/deploy.sh
+++ tests/integration/sepolia/deploy.sh
@@ -104,8 +104,8 @@ COLORS=$(jq -r '.colors[]' $CANVAS_CONFIG | sed 's/^/0x/')
 VOTABLE_COLOR_COUNT=$(jq -r '.votableColors[]' $CANVAS_CONFIG | wc -l | tr -d ' ')
 VOTABLE_COLORS=$(jq -r '.votableColors[]' $CANVAS_CONFIG | sed 's/^/0x/')
 DAILY_NEW_COLORS_COUNT=3
-START_TIME=1620276200
-END_TIME=1720276200
+START_TIME=1720399517
+END_TIME=1721399517
 DEVNET_MODE=0
 
 DAILY_QUESTS_COUNT=$(jq -r '.daily.dailyQuestsCount' $QUESTS_CONFIG)
