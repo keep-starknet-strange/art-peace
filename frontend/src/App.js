f554cb97868e320b9d4b64f18dfbb10b25107111
Brandon Roberts <brandonjroberts22@gmail.com>
2025-01-17 21:47:15 -0600

    Place single pixel call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1369,6 +1369,7 @@ function App() {
           colorPixel={colorPixel}
           worldsMode={worldsMode}
           openedWorldId={openedWorldId}
+          placeWorldPixelCall={placeWorldPixelCall}
           activeWorld={activeWorld}
           width={width}
           height={height}
